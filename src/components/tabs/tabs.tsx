34fa237c6d6b8d40b24cc9adf9fa0c0f0ee824ea
slipfly <slipfly@gmail.com>
2024-03-05 15:20:31 +0300

    создан компонент вкладок городов

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,26 @@
+type TabsProps = {
+  cities: string[];
+};
+
+function GetTabs({cities}: TabsProps) {
+  const citiesList = cities.map((city) =>
+    `<li className="locations__item">
+      <a className="locations__item-link tabs__item" href="#">
+        <span>${city}</span>
+      </a>
+    </li>`
+  );
+  return citiesList;
+}
+
+export default function Tabs({cities}: TabsProps): JSX.Element {
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          <GetTabs cities={cities} />
+        </ul>
+      </section>
+    </div>
+  );
+}
